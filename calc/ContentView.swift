b8bfae5568356bde4feff223c73d7a533f22a107
YaroDE-ops <yaro.demeulenaere@jopie.be>
2025-02-20 10:49:18 +0100

    Initial Commit

---
+++ calc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calc
+//
+//  Created by yaro demeulenaere on 20/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calc/calcApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcApp.swift
+//  calc
+//
+//  Created by yaro demeulenaere on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct calcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
