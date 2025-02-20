1e74284c99eedb65764ee04d838bd9b66c88f91d
YaroDE-ops <yaro.demeulenaere@jopie.be>
2025-02-20 11:00:54 +0100

    Hallo toegevoegd

--- calc/ContentView.swift
+++ calc/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Divider()
+            Spacer()
+            Text("Hallo")
+            
+        }.background(Color.black).foregroundColor(.white)
+        
     }
 }
 
